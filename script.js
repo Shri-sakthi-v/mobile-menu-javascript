ad336f9d7cd9052d9bee1fa2d1a3a1e1d7f87f03
Tshepo45 <97479621+Tshepo45@users.noreply.github.com>
2023-03-14 11:27:42 +0200

    Update script.js
    Modified .js script

--- script.js
+++ script.js
@@ -16,6 +16,35 @@ hamburger.addEventListener('click', () => {
     textDecoration: 'none',
   };
 
-  
-  
+  });
+div.appendChild(img);
+div.appendChild(links);
+divWrap.append(div);
+home.appendChild(divWrap);
+ divWrap.style.backgroundColor = 'rgba(96, 112, 255,0.9)';
+img.src = 'assets/Toolbar.png';
+img.style.float = 'right';
+img.style.marginRight = '4%';
+img.addEventListener('click', () => {
+  img.style.cursor = 'pointer';
 });
+links.style.display = 'flex';
+links.style.flexDirection = 'column';
+links.style.clear = 'both';
+divWrap.style.zIndex = '1';
+divWrap.style.position = 'fixed';
+divWrap.style.top = '0';
+divWrap.style.height = '130vh';
+divWrap.style.width = '100%';
+div.style.marginTop = '40px';
+links.style.listStyle = 'none';
+img.addEventListener('click', () => {
+  divWrap.style.display = 'none';
+});
+div.appendChild(img);
+div.appendChild(links);
+divWrap.append(div);
+home.appendChild(divWrap);
+
+});
+
