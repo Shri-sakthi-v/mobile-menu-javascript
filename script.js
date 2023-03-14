2cdfa5d554e0fe4704a7dde1c1ecc5c23f584f09
sakthi <shrisakthi.ind@gmail.com>
2023-03-14 14:23:02 +0530

    add js file

---
+++ script.js
@@ -0,0 +1,21 @@
+const hamburger = document.querySelector('.navigator_menu');
+
+hamburger.addEventListener('click', () => {
+  const div = document.createElement('div');
+  const img = document.createElement('img');
+  const links = document.querySelector('.internal-link');
+  const home = document.querySelector('.starting-page');
+  const divWrap = document.createElement('div');
+  const linksChilds = Array.from(links.children);
+  const property = {
+    color: '#fff',
+    fontSize: '32px',
+    fontFamily: 'Poppins sans-serif',
+    fontWeight: '600px',
+    lineHeight: '44px',
+    textDecoration: 'none',
+  };
+
+  
+  
+});
