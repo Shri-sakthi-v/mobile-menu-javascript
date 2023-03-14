68583a337f41699f79f839d984128a3fbb60b70f
sakthi <shrisakthi.ind@gmail.com>
2023-03-14 15:15:43 +0530

    update the js file

--- script.js
+++ script.js
@@ -16,45 +16,41 @@ hamburger.addEventListener('click', () => {
     textDecoration: 'none',
   };
 
+  divWrap.style.backgroundColor = 'rgba(96, 112, 255,0.9)';
+  img.src = 'assets/Toolbar.png';
+  img.style.float = 'right';
+  img.style.marginRight = '4%';
+  img.addEventListener('click', () => {
+    img.style.cursor = 'pointer';
   });
-div.appendChild(img);
-div.appendChild(links);
-divWrap.append(div);
-home.appendChild(divWrap);
- divWrap.style.backgroundColor = 'rgba(96, 112, 255,0.9)';
-img.src = 'assets/Toolbar.png';
-img.style.float = 'right';
-img.style.marginRight = '4%';
-img.addEventListener('click', () => {
-  img.style.cursor = 'pointer';
-});
-links.style.display = 'flex';
-links.style.flexDirection = 'column';
-links.style.clear = 'both';
-divWrap.style.zIndex = '1';
-divWrap.style.position = 'fixed';
-divWrap.style.top = '0';
-divWrap.style.height = '130vh';
-divWrap.style.width = '100%';
-div.style.marginTop = '40px';
-links.style.listStyle = 'none';
-img.addEventListener('click', () => {
-  divWrap.style.display = 'none';
-});
-div.appendChild(img);
-div.appendChild(links);
-divWrap.append(div);
-home.appendChild(divWrap);
-
-for (let i = 0; i < linksChilds.length; i += 1) {
-  const elementChildren = linksChilds[i].children;
-  elementChildren[0].addEventListener('click', () => {
+  links.style.display = 'flex';
+  links.style.flexDirection = 'column';
+  links.style.clear = 'both';
+  divWrap.style.zIndex = '1';
+  divWrap.style.position = 'fixed';
+  divWrap.style.top = '0';
+  divWrap.style.height = '130vh';
+  divWrap.style.width = '100%';
+  div.style.marginTop = '40px';
+  links.style.listStyle = 'none';
+  img.addEventListener('click', () => {
     divWrap.style.display = 'none';
   });
-  for (let j = 0; j < Object.keys(property).length; j += 1) {
-    elementChildren[0].style[Object.keys(property)[j]] = property[Object.keys(property)[j]];
-  }
-  console.log(elementChildren);
-}
+
+  div.appendChild(img);
+  div.appendChild(links);
+  divWrap.append(div);
+  home.appendChild(divWrap);
+
+  for (let i = 0; i < linksChilds.length; i += 1) {
+    const elementChildren = linksChilds[i].children;
+    elementChildren[0].addEventListener('click', () => {
+        divWrap.style.display = 'none';
+    });
+
+    for (let j = 0; j < Object.keys(property).length; j += 1) {
+      elementChildren[0].style[Object.keys(property)[j]] = property[Object.keys(property)[j]];
+     }
+     }
 });
 
