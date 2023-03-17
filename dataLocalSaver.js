2c8d43fc9b4a7c0cebe47ffcc210cc0cd9c38c43
Tshepo45 <97479621+Tshepo45@users.noreply.github.com>
2023-03-17 17:33:19 +0200

    Update dataLocalSaver.js

--- dataLocalSaver.js
+++ dataLocalSaver.js
@@ -1,4 +1,14 @@
-
+const userName = form.elements.name;
+const userEmail = form.elements.email;
+const userMessage = form.elements.message;
+function localstorage() {
+  const userInput = {
+    name: form.elements.name.value,
+    email: form.elements.email.value,
+    message: form.elements.message.value,
+  };
+  localStorage.setItem('userInput', JSON.stringify(userInput));
+}
 if (!localStorage.getItem('userInput')) {
   localstorage();
 } else {
